fc183f451b4f299087063fc894681ee7e4056ea4
AShrivastava <AShrivastava@in.imshealth.com>
2020-03-03 11:01:08 +0530

    Initial commit for python programming

--- src/resurces/MethodnObject.py
+++ src/resurces/MethodnObject.py
@@ -80,3 +80,108 @@ def myfunc(*args):
 
 
 myfunc(10,20)
+
+
+def myfunc(**kwargs):  ####dictonairy
+    if 'fruit' in kwargs:
+        print('my choice of fruit {}'.format(kwargs['fruit']))
+    else:
+        print('i didt not')
+
+
+myfunc(fruit = 'apple',veggie= 'lettuce')
+
+############# LESSER OF TWO EVENS:
+# #Write a function that returns the lesser of two given numbers
+# *if* both numbers are even, but returns the greater if one or both numbers are odd\n",
+
+
+def lesser_of_two_event(a,b):
+    if a%2 ==0 and b%2 ==0:
+        if a < b:
+            result = a
+        else:
+            result = b
+    else :
+        if a> b:
+            result= a
+        else:
+            result = b
+
+    return result
+
+
+print(lesser_of_two_event(2,5))
+
+
+def lesser_of_two_event(a,b):
+    if a%2 ==0 and b%2 ==0:
+       return  min(a,b)
+    else :
+     return max(a,b)
+
+    print(lesser_of_two_event(2, 5))
+
+
+####### ANIMAL CRACKERS:
+# #Write a function takes a two-word string and returns True if both words begin with same letter\n"
+
+def two_word(word1,word2):
+    if word1(0) == word2(0):
+        return True
+    else:
+        return False
+
+def two_word(text):
+    wordlist = text.lower().split()
+    print(wordlist)
+    return wordlist[0][0] == wordlist[1][0]
+
+print(two_word('Crazy cat'))
+
+
+#### MAKES TWENTY: Given two integers, return True if the sum of the integers is 20
+# *or* if one of the integers is 20. If not, return False\n",
+
+def makes_twenty(n1,n2):
+     return (n1+n2) == 20 or n1 == 20 or n2 == 20
+
+
+
+
+print(makes_twenty(3,2))
+
+
+########## OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name
+
+def  old_mac(name):
+    if len(name) > 3:
+        return name[:3].capitalize() + name[3:].capitalize()
+    else:
+        return 'name is too short'
+
+
+print(old_mac('macdonald'))
+
+
+###### MASTER YODA: Given a sentence, return a sentence with the words reversed
+#master_yoda('I am home') --> 'home am I'\n",
+# master_yoda('We are ready') --> 'ready are We'"
+
+
+def master_yoda(text):
+    wordlist = text.split()
+    reversed_word_list = wordlist[::-1]
+    return reversed_word_list
+
+print(master_yoda('I am home') )
+
+#####USING JOIN#######
+
+
+
+
+
+
+
+
